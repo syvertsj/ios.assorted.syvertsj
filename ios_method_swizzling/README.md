c790c8768986d852120bad75a69a4303f77075be
syvertsj <syvertsj@gmail.com>
2020-03-10 21:56:10 -0400

    ios method swizzling

---
+++ ios_method_swizzling/README.md
@@ -0,0 +1,5 @@
+# iOS: Method Swizzling For Accessibility Identification
+
+Exploit objc method swizzling to replace UIViewController.viewDidLoad() with custom version.
+
+Extensions to UIViewController and UIView enable labeling UIView elements with accessibility id strings.
---
+++ ios_method_swizzling/ios_method_swizzlingTests/ios_method_swizzlingTests.swift
@@ -0,0 +1,34 @@
+//
+//  ios_method_swizzlingTests.swift
+//  ios_method_swizzlingTests
+//
+//  Created by James on 3/10/20.
+//  Copyright © 2020 James. All rights reserved.
+//
+
+import XCTest
+@testable import ios_method_swizzling
+
+class ios_method_swizzlingTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
