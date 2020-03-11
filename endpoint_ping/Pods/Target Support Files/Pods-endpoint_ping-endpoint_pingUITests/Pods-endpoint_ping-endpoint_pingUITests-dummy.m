1705829527206ab05773ee77bb511ea77ef284b5
syvertsj <syvertsj@gmail.com>
2020-03-11 02:00:57 -0400

    ios ping endpoint

---
+++ endpoint_ping/Pods/Target Support Files/Pods-endpoint_ping-endpoint_pingUITests/Pods-endpoint_ping-endpoint_pingUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_endpoint_ping_endpoint_pingUITests : NSObject
+@end
+@implementation PodsDummy_Pods_endpoint_ping_endpoint_pingUITests
+@end
